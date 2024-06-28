46180f8b0a6b78086bd3f5563f52a1e94559e574
Kreendels285 <matvejm634@ya.ru>
2024-06-28 13:44:17 +0400

    21321

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,4 @@
 function PageLoaded() {
     console.log("Страница загружена | Page loaded");
 }
+
--- JS/scriptNIKO.js
+++ JS/scriptNIKO.js
@@ -1,3 +1,8 @@
 function PageLoaded() {
     console.log("Страница загружена | Page loaded");
 }
+
+function SetImg(sel_url) {
+    var item = document.getElementById('preview');
+    item.src=sel_url;
+}
